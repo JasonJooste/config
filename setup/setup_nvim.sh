8a17df94255e6f562a481d4c789bfdc2015b07db
Jason Jooste <jason.jooste@data61.csiro.au>
2023-03-16 15:54:40 +1000

    Add curl import to setup_nvim.sh

--- setup/setup_nvim.sh
+++ setup/setup_nvim.sh
@@ -1,6 +1,6 @@
 # A script for installing neovim and plugins. The ~/.config/nvim/coc-settings.json and ~/.config/nvim/init.vim files are assumed to already be populated from the git config repo.
 apt update
-apt install neovim -y
+apt install neovim curl -y
 # Install vim-plug
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 # Install plugins
