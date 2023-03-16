cf15b13b21a639a3829870c14639438606d4ae1f
Jason Jooste <jason.jooste@data61.csiro.au>
2023-03-16 15:36:06 +1000

    Add nvim setup script

---
+++ setup/setup_nvim.sh
@@ -0,0 +1,8 @@
+# A script for installing neovim and plugins. The ~/.config/nvim/coc-settings.json and ~/.config/nvim/init.vim files are assumed to already be populated from the git config repo.
+apt update
+apt install neovim -y
+# Install vim-plug
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# Install plugins
+nvim --headless +'PlugInstall --sync' +'qa'
+
