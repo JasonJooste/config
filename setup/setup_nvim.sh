136d686a822b4f636983d19f44798cf6453a66f6
Jason Jooste <jason.jooste@data61.csiro.au>
2023-05-04 11:51:11 +1000

    Modify setup_nvim to correctly assign permissions

--- setup/setup_nvim.sh
+++ setup/setup_nvim.sh
@@ -1,10 +1,11 @@
 # A script for installing neovim and plugins. The ~/.config/nvim/coc-settings.json and ~/.config/nvim/init.vim files are assumed to already be populated from the git config repo.
-apt update
-apt install neovim curl -y
+sudo apt update
+sudo apt install curl -y
+sudo snap install nvim --classic
 # Install vim-plug
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}/nvim/site/autoload/plug.vim" --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 # Install nodejs
-bash <(curl -sL install-node.vercel.app/lts) -y
+sudo bash <(curl -sL install-node.vercel.app/lts) -y
 # Install plugins
-nvim --headless -c 'PlugInstall --sync' -c 'qa'
+nvim --headless -c "PlugInstall --sync" -c qa
 
