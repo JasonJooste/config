08e14999bd4359e732161632eabf8033e39f649c
Jason Jooste <jason.jooste@data61.csiro.au>
2023-05-15 14:15:52 +1000

    Add docker engine, tmux and vim to the setup

---
+++ .setup/setup_tmux.sh
@@ -0,0 +1,5 @@
+sudo apt install tmux
+# Clone down the tmux-plugins repo
+rm -rf ~/.tmux/plugins/tpm
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
---
+++ .setup/setup_vim.sh
@@ -0,0 +1,4 @@
+sudo apt update
+sudo apt install curl
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
