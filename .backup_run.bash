3996d36ed0ffbf0a71962d3562347ae67c8706e1
Jason <jooste.jason@gmail.com>
2024-10-20 15:32:11 +0200

    Add logging output for successful or failed runs to backup

--- .backup_run.bash
+++ .backup_run.bash
@@ -1,3 +1,9 @@
 #!/usr/bin/bash
 HOME_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 /usr/bin/rclone sync --exclude-from "$HOME_DIR/.backup_exclude" "$HOME_DIR/Documents" backblaze-backup:portable-ubuntu-backup
+code=$?
+if [[ $code == 0 ]]; then
+	echo "Backup finished at $(date)"
+else
+	echo "Backup failed at $(date) with code $code"
+fi
