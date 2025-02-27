e445366b75ce14458875efb2d2858abf3cca2c52
Jason Jooste <jooste.jason@gmail.com>
2025-02-27 12:52:19 +0000

    Add venv to python install

--- .setup/cmdline/python-install.sh
+++ .setup/cmdline/python-install.sh
@@ -1,3 +1,3 @@
 # Get the deadsnakes ppa and install python 3.10
-sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt install python3.10
+sudo add-apt-repository -y ppa:deadsnakes/ppa
+sudo apt install -y python3.10 python3.10-venv python3-venv
