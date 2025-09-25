ce3dcbbc072b1ed0cc1f536e87a6aa29cada9c2c
Jason Jooste <jooste.jason@gmail.com>
2025-09-25 11:00:05 +0000

    Add pipx to python install

--- .setup/cmdline/python-install.sh
+++ .setup/cmdline/python-install.sh
@@ -1,3 +1,7 @@
 # Get the deadsnakes ppa and install python 3.10
 sudo add-apt-repository -y ppa:deadsnakes/ppa
 sudo apt install -y python3.10 python3.10-venv python3-venv
+# Install pipx for global installs in venvs
+sudo apt install pipx
+pipx ensurepath
+sudo pipx ensurepath --global
