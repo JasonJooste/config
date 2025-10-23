b3936925ce540a38fe1756616d4e5c6ac805e250
Jason Jooste <jooste.jason@gmail.com>
2025-10-23 12:34:33 +0000

    Add poetry install to python setup

--- .setup/cmdline/python-install.sh
+++ .setup/cmdline/python-install.sh
@@ -5,3 +5,6 @@ sudo apt install -y python3.10 python3.10-venv python3-venv
 sudo apt install pipx
 pipx ensurepath
 sudo pipx ensurepath --global
+# Install the latest poetry
+pipx install poetry
+poetry completions bash >> ~/.bash_completion
